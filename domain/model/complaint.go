3d4b2cd525bf712fde4dc8c5b3a90c7672b541c3
NH <rudyrudy2103@gmail.com>
2022-12-17 14:44:23 +0900

    insert結果に自動採番のcomplaintIdがマッピングされないもんだいかいけつ

--- domain/model/complaint.go
+++ domain/model/complaint.go
@@ -2,7 +2,11 @@ package model
 
 // ぐち
 type Complaint struct {
-	ComplaintId   int
+	// ID, CreatedAt, UpdatedAt, DeletedAt が付与される
+	// => Error 1054: Unknown column 'created_at' in 'field list'のエラー
+	// gorm.Model
+	// タグ`gorm:"primaryKey"`を付与。goの構造体は、複数のタグがある場合は半角スペースで区切って記載
+	ComplaintId   int    `json:"complaintId" example:"56" gorm:"primaryKey"`
 	ComplaintText string `json:"complaintText" example:"勘弁してくれ!"`
 	AvatarId      string `json:"avatarId" example:"1"`
 }
